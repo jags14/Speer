ed4c5552f1be5d0874218e26772de1404a436c18
Jagpat Singh <jsingh14192@gmail.com>
2024-01-04 23:45:00 +0530

    added connection code to mongodb

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,63 @@
 const express = require('express');
-const http = require('http');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const PORT = process.env.PORT || 3000;
+const dotenv = require('dotenv');
+const cookieSession = require('cookie-session');
+dotenv.config();
+
+var corsOptions = {
+    origin: 'http://localhost:4000'
+}
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`Server listening on Port ${PORT}`);
+app.use(cors(corsOptions));
+// parse requests of content-type: application/json
+app.use(express.json());
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}));
+
+app.use(
+    cookieSession({
+        name: 'speer-session',
+        keys: ['cookie_secret'],
+        httpOnly: true
+    })
+)
+app.get('/', (req, res) => {
+    console.log(req);
+    res.send("<h1>Hello world !</h1>")
+})
+
+// connecting to mongodb database
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://speer:" + process.env.MONGODB_PASSWORD + "@cluster0.qlrwiez.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+// listen to server
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on Port ${process.env.PORT}`);
 });
